57ea663df3bd895a2f90dc4cec1b377c9b2394ea
James Costello <jmc@Jamess-MacBook-Pro.local>
2020-11-29 12:09:00 -0700

    add questions

--- index.js
+++ index.js
@@ -1,9 +1,82 @@
 'use strict';
 
+const inquier = require('inquirer');
+
+const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
-console.log(Engineer);
-console.log(Intern);
-console.log(Manager);
+const addManager = () => {
+  return inquier.prompt([
+    {
+      message: 'Enter manager name',
+      name: 'name',
+    },
+    {
+      type: 'number',
+      message: 'Enter manager ID',
+      name: 'manId',
+    },
+    {
+      message: 'Enter manager email',
+      name: 'email',
+    },
+    {
+      type: 'number',
+      message: 'Enter manager office number',
+      name: 'officeNumber',
+    }
+  ]);
+};
+
+const addEngineer = () => {
+  return inquier.prompt([
+    {
+      message: 'Enter engineer name',
+      name: 'name',
+    },
+    {
+      type: 'number',
+      message: 'Enter engineer ID',
+      name: 'engId',
+    },
+    {
+      message: 'Enter engineer email',
+      name: 'email',
+    },
+    {
+      message: 'Enter engineer github',
+      name: 'github',
+    }
+  ]);
+};
+
+const addIntern = () => {
+  return inquier.prompt([
+    {
+      message: 'Enter intern name',
+      name: 'name',
+    },
+    {
+      type: 'number',
+      message: 'Enter intern ID',
+      name: 'intId',
+    },
+    {
+      message: 'Enter intern email',
+      name: 'email',
+    },
+    {
+      message: 'Enter intern school',
+      name: 'school',
+    }
+  ]);
+};
+
+addManager()
+  .then(addEngineer)
+  .then(addIntern)
+  .catch(err => {
+    console.log(err);
+  });
