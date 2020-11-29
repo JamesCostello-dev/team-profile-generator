657e619604a9562436d0053096fccdac6a85bb81
James Costello <jmc@Jamess-MacBook-Pro.local>
2020-11-29 11:20:22 -0700

    create classes

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 'use strict';
 
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+console.log(Engineer);
+console.log(Intern);
+console.log(Manager);
